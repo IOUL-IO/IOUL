4d44970edad7b2587d3c153d2ba7cb8aa3582c61
IOUL-IO <info@ioul.io>
2025-07-03 10:28:50 -0700

    Add files via upload

---
+++ pages/tool-kit/ops/index.tsx
@@ -0,0 +1,24 @@
+import { promises as fs } from 'fs';
+import path from 'path';
+import Head from 'next/head';
+
+type PageProps = {
+  html: string;
+};
+
+export async function getStaticProps() {
+  const filePath = path.join(process.cwd(), 'public', `tool-kit/ops/index.html`);
+  const html = await fs.readFile(filePath, 'utf-8');
+  return { props: { html } };
+}
+
+export default function StaticWrappedPage({ html }: PageProps) {
+  return (
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+      </Head>
+      <div dangerouslySetInnerHTML={ { __html: html } } />
+    </>
+  );
+}

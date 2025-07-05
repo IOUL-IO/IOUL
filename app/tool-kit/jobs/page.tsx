5be5d7794769f35fd36c7bdd10a289785ec7857a
IOUL-IO <info@ioul.io>
2025-07-04 22:09:14 -0700

    Add files via upload

--- app/tool-kit/jobs/page.tsx
+++ app/tool-kit/jobs/page.tsx
@@ -6,7 +6,22 @@
       useEffect(() => {
         // TODO: any JS init from legacy project can be ported here
       }, []);
-      return (
+      
+useEffect(() => {
+  // lock scrollbars when Jobs page mounts
+  const origHtmlOverflow = document.documentElement.style.overflow;
+  const origBodyOverflow = document.body.style.overflow;
+  document.body.classList.add('non-fullscreen');
+  document.documentElement.style.overflow = 'hidden';
+  document.body.style.overflow = 'hidden';
+  return () => {
+    document.body.classList.remove('non-fullscreen');
+    document.documentElement.style.overflow = origHtmlOverflow;
+    document.body.style.overflow = origBodyOverflow;
+  };
+}, []);
+
+  return (
         <div dangerouslySetInnerHTML={ { __html: `
 
   <!-- Fixed white mask layers -->

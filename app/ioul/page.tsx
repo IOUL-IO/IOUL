2e90cfb8de7cbd9a3cc0e353bd58cb79658c87b2
IOUL-IO <info@ioul.io>
2025-07-05 21:37:40 -0700

    Add files via upload

--- app/ioul/page.tsx
+++ app/ioul/page.tsx
@@ -1,12 +1,19 @@
 
     "use client";
     import React, { useEffect } from 'react';
+import Head from 'next/head';
+import Script from 'next/script';
 
     export default function Page() {
       useEffect(() => {
         // TODO: any JS init from legacy project can be ported here
       }, []);
       return (
+    <>
+      <Head>
+        <link rel="stylesheet" href="/IOUL-login/ioul/styles.css" />
+      </Head>
+      <Script src="/IOUL-login/ioul/script.js" strategy="afterInteractive" />
         <div dangerouslySetInnerHTML={ { __html: `<p style="display:none" lang="en">This page is already in English. No translation is needed.</p>
 
   <div class="layer-one"></div>

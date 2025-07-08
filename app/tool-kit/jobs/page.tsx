52066a533dffbe4f7cfbe388b908d5c15dcd92f0
IOUL-IO <info@ioul.io>
2025-07-08 16:58:15 -0700

    Update page.tsx

--- app/tool-kit/jobs/page.tsx
+++ app/tool-kit/jobs/page.tsx
@@ -180,81 +180,432 @@ export default function Page() {
         <div className="line util-line" />
 
         {/* Job items */}
-<span className="custom-text job-item" style={{ position: 'absolute', top: '35.4vh', left: '6.41vw' }}>JOB LOg</span>
-<span className="custom-text job-item" style={{ position: 'absolute', top: '41.6vh', left: '6.41vw' }}>APPL1ED</span>
-<span className="custom-text job-item" style={{ position: 'absolute', top: '53vh',   left: '6.41vw' }}>QUAL1FY</span>
-<span className="custom-text job-item" style={{ position: 'absolute', top: '59.2vh', left: '6.41vw' }}>1n1T1ATE</span>
+{/* Job items */}
+<span
+  className="custom-text job-item"
+  style={{ position: 'absolute', top: '35.4vh', left: '6.41vw' }}
+>
+  JOB LOg
+</span>
+<span
+  className="custom-text job-item"
+  style={{ position: 'absolute', top: '41.6vh', left: '6.41vw' }}
+>
+  APPL1ED
+</span>
+<span
+  className="custom-text job-item"
+  style={{ position: 'absolute', top: '53vh', left: '6.41vw' }}
+>
+  QUAL1FY
+</span>
+<span
+  className="custom-text job-item"
+  style={{ position: 'absolute', top: '59.2vh', left: '6.41vw' }}
+>
+  1n1T1ATE
+</span>
 
 {/* Job counters */}
-<span className="custom-text right-flow job-item" style={{ position: 'absolute', top: '35.4vh', left: '28.41vw' }}>0</span>
-<span className="custom-text right-flow job-item" style={{ position: 'absolute', top: '41.6vh', left: '28.41vw' }}>0</span>
-<span className="custom-text right-flow job-item" style={{ position: 'absolute', top: '53vh',   left: '28.41vw' }}>0</span>
-<span className="custom-text right-flow job-item" style={{ position: 'absolute', top: '59.2vh', left: '28.41vw' }}>0</span>
+<span
+  className="custom-text right-flow job-item"
+  style={{ position: 'absolute', top: '35.4vh', left: '28.41vw' }}
+>
+  0
+</span>
+<span
+  className="custom-text right-flow job-item"
+  style={{ position: 'absolute', top: '41.6vh', left: '28.41vw' }}
+>
+  0
+</span>
+<span
+  className="custom-text right-flow job-item"
+  style={{ position: 'absolute', top: '53vh', left: '28.41vw' }}
+>
+  0
+</span>
+<span
+  className="custom-text right-flow job-item"
+  style={{ position: 'absolute', top: '59.2vh', left: '28.41vw' }}
+>
+  0
+</span>
 
-        {/* Divider line */}
+{/* Divider line that used to slide with the containers */}
 <div className="custom-line job-item" />
 
-{/* Job‐line bars */}
-<div className="job-line job-line-one" style={{ position: 'absolute', top: '47.8vh', left: '36vw', width: '36vw' }} />
-<div className="job-line job-line-two" style={{ position: 'absolute', top: '47.8vh', left: '79vw', width: '14.8vw' }} />
+{/* Item lines */}
+<div
+  className="job-line job-line-one"
+  style={{ position: 'absolute', top: '47.8vh', left: '36vw', width: '36vw' }}
+/>
+<div
+  className="job-line job-line-two"
+  style={{ position: 'absolute', top: '47.8vh', left: '79vw', width: '14.8vw' }}
+/>
 
-        {/* Freelance lines */}
-<div className="freelance-line freelance-line-one"   style={{ position: 'absolute', top: '47.8vh', left: '106.0vw', width: '36vw' }} />
-<div className="freelance-line freelance-line-two"   style={{ position: 'absolute', top: '47.8vh', left: '149.0vw', width: '14.8vw' }} />
+{/* Center lines */}
+<div
+  className="freelance-line freelance-line-one"
+  style={{ position: 'absolute', top: '47.8vh', left: '106.0vw', width: '36vw' }}
+/>
+<div
+  className="freelance-line freelance-line-two"
+  style={{ position: 'absolute', top: '47.8vh', left: '149.0vw', width: '14.8vw' }}
+/>
 
-{/* Freelance texts & counters */}
-<span className="freelance-text"            style={{ position: 'absolute', top: '35.4vh', left: '106.0vw' }}>LOOK UP:</span>
-<span className="freelance-text"            style={{ position: 'absolute', top: '41.6vh', left: '106.0vw' }}>JOB LOg:</span>
-<span className="custom-text right-flow freelance-text" style={{ position: 'absolute', top: '35.4vh', left: '119.7vw' }}>0</span>
-<span className="custom-text right-flow freelance-text" style={{ position: 'absolute', top: '41.6vh', left: '119.7vw' }}>0</span>
+{/* Center texts */}
+<span
+  className="freelance-text"
+  style={{ position: 'absolute', top: '35.4vh', left: '106.0vw' }}
+>
+  LOOK UP:
+</span>
+<span
+  className="freelance-text"
+  style={{ position: 'absolute', top: '41.6vh', left: '106.0vw' }}
+>
+  JOB LOg:
+</span>
 
-<span className="freelance-text"            style={{ position: 'absolute', top: '35.4vh', left: '128.0vw' }}>PER1OD:</span>
-<span className="freelance-text"            style={{ position: 'absolute', top: '41.6vh', left: '128.0vw' }}>F1LTER:</span>
-<span className="custom-text right-flow freelance-text" style={{ position: 'absolute', top: '35.4vh', left: '141.0vw' }}>0</span>
-<span className="custom-text right-flow freelance-text" style={{ position: 'absolute', top: '41.6vh', left: '141.0vw' }}>0</span>
+<span
+  className="freelance-text right-flow"
+  style={{ position: 'absolute', top: '35.4vh', left: '119.7vw' }}
+>
+  0
+</span>
+<span
+  className="freelance-text right-flow"
+  style={{ position: 'absolute', top: '41.6vh', left: '119.7vw' }}
+>
+  0
+</span>
 
-        {/* Extra job-text columns */}
-<span className="job-text"       style={{ position: 'absolute', top: '35.4vh', left: '36vw' }}>LOOK UP:</span>
-<span className="job-text"       style={{ position: 'absolute', top: '41.6vh', left: '36vw' }}>OPT FOR:</span>
-<span className="job-text right-flow" style={{ position: 'absolute', top: '35.4vh', left: '49.7vw' }}>0</span>
-<span className="job-text right-flow" style={{ position: 'absolute', top: '41.6vh', left: '49.7vw' }}>0</span>
+<span
+  className="freelance-text"
+  style={{ position: 'absolute', top: '35.4vh', left: '128.0vw' }}
+>
+  PER1OD:
+</span>
+<span
+  className="freelance-text"
+  style={{ position: 'absolute', top: '41.6vh', left: '128.0vw' }}
+>
+  F1LTER:
+</span>
 
-<span className="job-text"       style={{ position: 'absolute', top: '35.4vh', left: '58vw' }}>PER1OD:</span>
-<span className="job-text"       style={{ position: 'absolute', top: '41.6vh', left: '58vw' }}>F1LTER:</span>
-<span className="job-text right-flow" style={{ position: 'absolute', top: '35.4vh', left: '71vw' }}>0</span>
-<span className="job-text right-flow" style={{ position: 'absolute', top: '41.6vh', left: '71vw' }}>0</span>
+<span
+  className="freelance-text right-flow"
+  style={{ position: 'absolute', top: '35.4vh', left: '141.0vw' }}
+>
+  0
+</span>
+<span
+  className="freelance-text right-flow"
+  style={{ position: 'absolute', top: '41.6vh', left: '141.0vw' }}
+>
+  0
+</span>
 
-<span className="job-text"       style={{ position: 'absolute', top: '35.4vh', left: '79vw' }}>AcT1VE</span>
-<span className="job-text"       style={{ position: 'absolute', top: '41.6vh', left: '79vw' }}>JO1nED</span>
-<span className="job-text right-flow" style={{ position: 'absolute', top: '35.4vh', left: '93.4vw' }}>0</span>
-<span className="job-text right-flow" style={{ position: 'absolute', top: '41.6vh', left: '93.4vw' }}>0</span>
+<span
+  className="freelance-text"
+  style={{ position: 'absolute', top: '35.4vh', left: '149.0vw' }}
+>
+  RAT1ngS
+</span>
+<span
+  className="freelance-text"
+  style={{ position: 'absolute', top: '41.6vh', left: '149.0vw' }}
+>
+  REcE1PT
+</span>
 
-        {/* Community container */}
+<span
+  className="freelance-text right-flow"
+  style={{ position: 'absolute', top: '53vh', left: '163.4vw' }}
+>
+  0
+</span>
+<span
+  className="freelance-text right-flow"
+  style={{ position: 'absolute', top: '59.2vh', left: '163.4vw' }}
+>
+  0
+</span>
+
+<span
+  className="freelance-text"
+  style={{ position: 'absolute', top: '53vh', left: '149.0vw' }}
+>
+  OR:
+</span>
+<span
+  className="freelance-text"
+  style={{ position: 'absolute', top: '59.2vh', left: '149.0vw' }}
+>
+  OR:
+</span>
+<span
+  className="freelance-text"
+  style={{ position: 'absolute', top: '65.4vh', left: '149.0vw' }}
+>
+  OR:
+</span>
+<span
+  className="freelance-text"
+  style={{ position: 'absolute', top: '71.6vh', left: '149.0vw' }}
+>
+  OR:
+</span>
+
+<span
+  className="freelance-text right-flow"
+  style={{ position: 'absolute', top: '53vh', left: '163.4vw' }}
+>
+  0
+</span>
+<span
+  className="freelance-text right-flow"
+  style={{ position: 'absolute', top: '59.2vh', left: '163.4vw' }}
+>
+  0
+</span>
+<span
+  className="freelance-text right-flow"
+  style={{ position: 'absolute', top: '65.4vh', left: '163.4vw' }}
+>
+  0
+</span>
+<span
+  className="freelance-text right-flow"
+  style={{ position: 'absolute', top: '71.6vh', left: '163.4vw' }}
+>
+  0
+</span>
+
+{/* Item texts */}
+<span
+  className="job-text"
+  style={{ position: 'absolute', top: '35.4vh', left: '36vw' }}
+>
+  LOOK UP:
+</span>
+<span
+  className="job-text"
+  style={{ position: 'absolute', top: '41.6vh', left: '36vw' }}
+>
+  OPT FOR:
+</span>
+
+<span
+  className="job-text right-flow"
+  style={{ position: 'absolute', top: '35.4vh', left: '49.7vw' }}
+>
+  0
+</span>
+<span
+  className="job-text right-flow"
+  style={{ position: 'absolute', top: '41.6vh', left: '49.7vw' }}
+>
+  0
+</span>
+
+<span
+  className="job-text"
+  style={{ position: 'absolute', top: '35.4vh', left: '58vw' }}
+>
+  PER1OD:
+</span>
+<span
+  className="job-text"
+  style={{ position: 'absolute', top: '41.6vh', left: '58vw' }}
+>
+  F1LTER:
+</span>
+
+<span
+  className="job-text right-flow"
+  style={{ position: 'absolute', top: '35.4vh', left: '71vw' }}
+>
+  0
+</span>
+<span
+  className="job-text right-flow"
+  style={{ position: 'absolute', top: '41.6vh', left: '71vw' }}
+>
+  0
+</span>
+
+<span
+  className="job-text"
+  style={{ position: 'absolute', top: '35.4vh', left: '79vw' }}
+>
+  AcT1VE
+</span>
+<span
+  className="job-text"
+  style={{ position: 'absolute', top: '41.6vh', left: '79vw' }}
+>
+  JO1nED
+</span>
+
+<span
+  className="job-text right-flow"
+  style={{ position: 'absolute', top: '35.4vh', left: '93.4vw' }}
+>
+  0
+</span>
+<span
+  className="job-text right-flow"
+  style={{ position: 'absolute', top: '41.6vh', left: '93.4vw' }}
+>
+  0
+</span>
+
+<span
+  className="job-text"
+  style={{ position: 'absolute', top: '53vh', left: '79vw' }}
+>
+  JA:
+</span>
+<span
+  className="job-text"
+  style={{ position: 'absolute', top: '59.2vh', left: '79vw' }}
+>
+  JA:
+</span>
+<span
+  className="job-text"
+  style={{ position: 'absolute', top: '65.4vh', left: '79vw' }}
+>
+  JA:
+</span>
+<span
+  className="job-text"
+  style={{ position: 'absolute', top: '71.6vh', left: '79vw' }}
+>
+  JL:
+</span>
+
+<span
+  className="job-text right-flow"
+  style={{ position: 'absolute', top: '53vh', left: '93.4vw' }}
+>
+  0
+</span>
+<span
+  className="job-text right-flow"
+  style={{ position: 'absolute', top: '59.2vh', left: '93.4vw' }}
+>
+  0
+</span>
+<span
+  className="job-text right-flow"
+  style={{ position: 'absolute', top: '65.4vh', left: '93.4vw' }}
+>
+  0
+</span>
+<span
+  className="job-text right-flow"
+  style={{ position: 'absolute', top: '71.6vh', left: '93.4vw' }}
+>
+  0
+</span>
+
+<span
+  className="job-text"
+  style={{ position: 'absolute', top: '53vh', left: '36vw' }}
+>
+  APW
+</span>
+<span
+  className="job-text"
+  style={{ position: 'absolute', top: '59.2vh', left: '36vw' }}
+>
+  AOR
+</span>
+
+<span
+  className="job-text right-flow"
+  style={{ position: 'absolute', top: '53vh', left: '49.7vw' }}
+>
+  0
+</span>
+<span
+  className="job-text right-flow"
+  style={{ position: 'absolute', top: '59.2vh', left: '49.7vw' }}
+>
+  0
+</span>
+
+<span
+  className="job-text"
+  style={{ position: 'absolute', top: '53vh', left: '58.7vw' }}
+>
+  A1R
+</span>
+<span
+  className="job-text"
+  style={{ position: 'absolute', top: '59.2vh', left: '58.7vw' }}
+>
+  AT1
+</span>
+
+<span
+  className="job-text right-flow"
+  style={{ position: 'absolute', top: '53vh', left: '71vw' }}
+>
+  0
+</span>
+<span
+  className="job-text right-flow"
+  style={{ position: 'absolute', top: '59.2vh', left: '71vw' }}
+>
+  0
+</span>
+
+{/* Community & Zero items */}
 <div className="freelance-items-container" style={{ position: 'absolute', zIndex: 1 }}>
-  {/* 4 labels */}
-  <span className="custom-text" style={{ position: 'absolute', top: '35.4vh', left: '35.41vw' }}>OFFER LOg</span>
-  <span className="custom-text" style={{ position: 'absolute', top: '41.6vh', left: '35.41vw' }}>ORDER LOg</span>
-  <span className="custom-text" style={{ position: 'absolute', top: '53vh',   left: '35.41vw' }}>HELP LOg</span>
-  <span className="custom-text" style={{ position: 'absolute', top: '59.2vh', left: '35.41vw' }}>JUnK LOg</span>
-
-  {/* horizontal divider */}
-  <div className="custom-line" style={{
-    position: 'absolute',
-    top: '47.8vh',
-    left: '35.41vw',
-    width: '22.48vw',
-    height: '1px',
-    background: 'rgba(230,230,230,0.28)'
-  }} />
+  <span className="custom-text" style={{ position: 'absolute', top: '35.4vh', left: '35.41vw' }}>
+    OFFER LOg
+  </span>
+  <span className="custom-text" style={{ position: 'absolute', top: '41.6vh', left: '35.41vw' }}>
+    ORDER LOg
+  </span>
+  <span className="custom-text" style={{ position: 'absolute', top: '53vh', left: '35.41vw' }}>
+    HELP LOg
+  </span>
+  <span className="custom-text" style={{ position: 'absolute', top: '59.2vh', left: '35.41vw' }}>
+    JUnK LOg
+  </span>
+  <div
+    className="custom-line"
+    style={{
+      position: 'absolute',
+      top: '47.8vh',
+      left: '35.41vw',
+      width: '22.48vw',
+      height: '1px',
+      background: 'rgba(230,230,230,0.28)',
+    }}
+  />
 </div>
-
-{/* Zero container */}
 <div className="zero-items-container" style={{ position: 'absolute', zIndex: 1 }}>
-  <span className="custom-text right-flow" style={{ position: 'absolute', top: '35.4vh', left: '57.4vw' }}>0</span>
-  <span className="custom-text right-flow" style={{ position: 'absolute', top: '41.6vh', left: '57.4vw' }}>0</span>
-  <span className="custom-text right-flow" style={{ position: 'absolute', top: '53vh',   left: '57.4vw' }}>0</span>
-  <span className="custom-text right-flow" style={{ position: 'absolute', top: '59.2vh', left: '57.4vw' }}>0</span>
+  <span className="custom-text right-flow" style={{ position: 'absolute', top: '35.4vh', left: '57.4vw' }}>
+    0
+  </span>
+  <span className="custom-text right-flow" style={{ position: 'absolute', top: '41.6vh', left: '57.4vw' }}>
+    0
+  </span>
+  <span className="custom-text right-flow" style={{ position: 'absolute', top: '53vh', left: '57.4vw' }}>
+    0
+  </span>
+  <span className="custom-text right-flow" style={{ position: 'absolute', top: '59.2vh', left: '57.4vw' }}>
+    0
+  </span>
 </div>
+
 
         {/* Full‐screen grid overlay */}      </div>
     </>

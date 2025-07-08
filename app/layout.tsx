cd6c1e996dbd8cccd5786cad903f423c9606f298
IOUL-IO <info@ioul.io>
2025-07-08 12:46:59 -0700

    Add files via upload

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,25 +3,7 @@ import type { Metadata } from 'next';
 import LegacyScripts from './legacy-scripts';
 
 const legacyCssFiles = [
-  '/IOUL-login/styles.css',
-  '/IOUL-login/center/add-ons/styles.css',
-  '/IOUL-login/center/community/styles.css',
-  '/IOUL-login/center/ioul center/styles.css',
-  '/IOUL-login/center/library/styles.css',
-  '/IOUL-login/ioul/styles.css',
-  '/IOUL-login/tool-kit/cms/styles.css',
-  '/IOUL-login/tool-kit/cms/docs/styles.css',
-  '/IOUL-login/tool-kit/cms/forms/styles.css',
-  '/IOUL-login/tool-kit/cms/sheets/styles.css',
-  '/IOUL-login/tool-kit/coms/styles.css',
-  '/IOUL-login/tool-kit/crm/styles.css',
-  '/IOUL-login/tool-kit/erp/styles.css',
-  '/IOUL-login/tool-kit/fms/styles.css',
-  '/IOUL-login/tool-kit/hr/styles.css',
-  '/IOUL-login/tool-kit/it/styles.css',
-  '/IOUL-login/tool-kit/jobs/styles.css',
-  '/IOUL-login/tool-kit/lms/styles.css',
-  '/IOUL-login/tool-kit/ops/styles.css',
+  '/IOUL-login/styles.css'
 ];
 
 export const metadata: Metadata = {

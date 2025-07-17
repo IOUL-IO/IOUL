7c2f4ab206fd71e9dc8bb6f7bfd6a1d755610d9f
IOUL-IO <info@ioul.io>
2025-07-17 12:13:08 -0700

    Add files via upload

--- app/ioul/page.tsx
+++ app/ioul/page.tsx
@@ -1,28 +1,31 @@
-// page.tsx
 "use client";
 
 import React, { useEffect, useState, useCallback } from "react";
 import "./styles.css";
 
 const IOULPage: React.FC = () => {
-  // 0 = baseline, 1 = mail, 2 = calendar
-  const [state, setState] = useState(0);
+  // UTIL LINE TOGGLE STATE: 0=baseline, 1=mail, 2=calendar
+  const [utilState, setUtilState] = useState(0);
 
+  // Advance state on each click
   const handleUtilLineClick = useCallback(() => {
-    setState((prev) => (prev + 1) % 3);
+    setUtilState((prev) => (prev + 1) % 3);
   }, []);
 
+  // Sync `data-util` for CSS toggles
   useEffect(() => {
-    document.documentElement.setAttribute("data-util", state.toString());
-  }, [state]);
+    document.documentElement.setAttribute("data-util", utilState.toString());
+  }, [utilState]);
 
   return (
     <div className="non-fullscreen" translate="no">
+      {/* Background layers */}
       <div className="layer-one" />
       <div className="layer-two" />
       <div className="layer-three" />
       <div className="layer-four" />
 
+      {/* Utility lines */}
       <div className="other-content">
         <div className="line original" />
         <div className="line second" />
@@ -30,30 +33,26 @@ const IOULPage: React.FC = () => {
         <div className="line third" />
         <div className="line fourth" />
         <div className="line fifth" />
-        {state === 1 && <div className="line mail-line" />}
+        <div className="line mail-line" />
         <div className="line sixth" />
       </div>
 
-      {state === 1 && (
-        <>
-          <span className="mail-text to">TO:</span>
-          <span className="mail-text subject">SUBJECT:</span>
-          <span className="mail-text cc">cc</span>
-          <span className="mail-text bcc">Bcc</span>
-          <span className="mail-text send">SEND</span>
-        </>
-      )}
+      {/* Mail labels */}
+      <span className="mail-text to">TO:</span>
+      <span className="mail-text subject">SUBJECT:</span>
+      <span className="mail-text cc">cc</span>
+      <span className="mail-text bcc">Bcc</span>
+      <span className="mail-text send">SEND</span>
 
-      {state === 2 && (
-        <>
-          {[...Array(16)].map((_, i) => (
-            <React.Fragment key={i}>
-              <span className={`grid-number num${i + 1}`}>{i + 1}</span>
-              <span className={`grid-dashed dashed${String(i + 1).padStart(2, "0")}`} />
-            </React.Fragment>
-          ))}
-        </>
-      )}
+      {/* Calendar grid 1–16 */}
+      {[...Array(16)].map((_, i) => (
+        <React.Fragment key={i}>
+          <span className={`grid-number num${i + 1}`}>{i + 1}</span>
+          <span
+            className={`grid-dashed dashed${String(i + 1).padStart(2, "0")}`}
+          />
+        </React.Fragment>
+      ))}
     </div>
   );
 };

8ff3f31ff4c01e2aa115cf844b84db3468d395c1
IOUL-IO <info@ioul.io>
2025-07-23 17:17:17 -0700

    Delete app directory

--- app/tool-kit/fms/head.tsx
+++
@@ -1,7 +0,0 @@
-export default function Head() {
-  return (
-    <>
-      <link rel="stylesheet" href="/IOUL-login/tool-kit/fms/styles.css" />
-    </>
-  );
-}
--- app/tool-kit/hr/head.tsx
+++
@@ -1,7 +0,0 @@
-export default function Head() {
-  return (
-    <>
-      <link rel="stylesheet" href="/IOUL-login/tool-kit/hr/styles.css" />
-    </>
-  );
-}

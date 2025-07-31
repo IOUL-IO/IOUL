237839b1765b2fa39c4bb1cc87a394403982ccab
IOUL-IO <info@ioul.io>
2025-07-30 19:17:20 -0700

    Update page.tsx

--- app/tool-kit/cms/forms/page.tsx
+++ app/tool-kit/cms/forms/page.tsx
@@ -41,8 +41,35 @@ export default function Page() {
       <div className="line fifth" />
       <div className="line sixth" />
 
-      {/* Util line (click-to-toggle reserved for future use) */}
       <div className="line util-line" />
+
+        <span className="custom-text" style={{ position: 'absolute', top: '35.4vh', left: '6.41vw' }}>
+          ADD QUEST1On
+        </span>
+        <span className="custom-text" style={{ position: 'absolute', top: '41.6vh', left: '6.41vw' }}>
+          1MPORT YOURS
+        </span>
+        <span className="custom-text" style={{ position: 'absolute', top: '53vh', left: '6.41vw' }}>
+          REPL1ES
+        </span>
+        <span className="custom-text" style={{ position: 'absolute', top: '59.2vh', left: '6.41vw' }}>
+          OPT1OnS
+        </span>
+
+        <span className="custom-text right-flow" style={{ position: 'absolute', top: '35.4vh', left: '28.41vw' }}>
+          0
+        </span>
+        <span className="custom-text right-flow" style={{ position: 'absolute', top: '41.6vh', left: '28.41vw' }}>
+          0
+        </span>
+        <span className="custom-text right-flow" style={{ position: 'absolute', top: '53vh', left: '28.41vw' }}>
+          0
+        </span>
+        <span className="custom-text right-flow" style={{ position: 'absolute', top: '59.2vh', left: '28.41vw' }}>
+          0
+        </span>
+
+        <div className="custom-line" />
     </div>
   );
 }

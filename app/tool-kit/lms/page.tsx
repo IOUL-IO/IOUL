efabdede6af19dbb0aac87fce53e07ec6a2f19b5
IOUL-IO <info@ioul.io>
2025-08-01 11:30:00 -0700

    Update page.tsx

--- app/tool-kit/lms/page.tsx
+++ app/tool-kit/lms/page.tsx
@@ -72,9 +72,9 @@ export default function Page() {
         <span className="custom-text content-text" style={{ position: 'absolute', top: '35.4vh', left: '79vw' }}>1nQU1RY:</span>
         <span className="custom-text content-text" style={{ position: 'absolute', top: '41.6vh', left: '79vw' }}>REPORT:</span>
         <span className="custom-text content-text" style={{ position: 'absolute', top: '53vh',   left: '79vw' }}>LEARn</span>
-        <span className="custom-text content-text" style={{ position: 'absolute', top: '59.2vh', left: '79vw' }}>1-LEAD</span>
+        <span className="custom-text content-text" style={{ position: 'absolute', top: '59.2vh', left: '79vw' }}>cOAcH</span>
         <span className="custom-text content-text" style={{ position: 'absolute', top: '65.4vh', left: '79vw' }}>1-QU1Z</span>
-        <span className="custom-text content-text" style={{ position: 'absolute', top: '71.6vh', left: '79vw' }}>On-JOB</span>
+        <span className="custom-text content-text" style={{ position: 'absolute', top: '71.6vh', left: '79vw' }}>1-L1VE</span>
 
         {/* Column 6: counters */}
         <span className="custom-text content-text right-flow" style={{ position: 'absolute', top: '35.4vh', left: '93.4vw' }}>0</span>

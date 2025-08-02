4eba1d059c2a208a95446b008a2f76879047bbeb
IOUL-IO <info@ioul.io>
2025-08-02 12:07:15 -0700

    Update page.tsx

--- app/tool-kit/lms/learn/page.tsx
+++ app/tool-kit/lms/learn/page.tsx
@@ -59,7 +59,9 @@ export default function Page() {
 
         <span className="custom-text" style={{position:'absolute',top:'53vh',left:'36vw',zIndex:4,fontFamily:"'Distill Expanded',sans-serif",color:'#111111',letterSpacing:'0.28vw',fontSize:'0.47rem',textShadow:'0.001rem 0.001rem 0 #717171,-0.001rem -0.001rem 0 #717171',}}>LEcTURE 1:</span>
       
-        <span className="custom-text" style={{position:'absolute',top:'53vh',left:'90vw',zIndex:4,fontFamily:"'Distill Expanded',sans-serif",color:'#111111',letterSpacing:'0.28vw',fontSize:'0.47rem',textShadow:'0.001rem 0.001rem 0 #717171,-0.001rem -0.001rem 0 #717171',}}>LOAD:</span>
+        <span className="custom-text" style={{position:'absolute',top:'53vh',left:'79vw',zIndex:4,fontFamily:"'Distill Expanded',sans-serif",color:'#111111',letterSpacing:'0.28vw',fontSize:'0.47rem',textShadow:'0.001rem 0.001rem 0 #717171,-0.001rem -0.001rem 0 #717171',}}>UPLOAD:</span>
+        <span className="custom-text" style={{position:'absolute',top:'53vh',left:'89vw',zIndex:4,fontFamily:"'Distill Expanded',sans-serif",color:'#111111',letterSpacing:'0.28vw',fontSize:'0.47rem',textShadow:'0.001rem 0.001rem 0 #717171,-0.001rem -0.001rem 0 #717171',}}>DELETE:</span>
+
       
     </div>
   );

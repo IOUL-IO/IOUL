96e9bf33496ce0040e5455de0311a70ace98a03e
IOUL-IO <info@ioul.io>
2025-08-01 20:41:39 -0700

    Update page.tsx

--- app/tool-kit/lms/learn/page.tsx
+++ app/tool-kit/lms/learn/page.tsx
@@ -44,7 +44,7 @@ export default function Page() {
       <div className="line util-line" />
 
         <span className="custom-text" style={{ position: 'absolute', top: '35.4vh', left: '6.41vw' }}>LEcTURE LOg</span>
-        <span className="custom-text" style={{ position: 'absolute', top: '41.6vh', left: '6.41vw' }}>LEARnER LOg</span>
+        <span className="custom-text" style={{ position: 'absolute', top: '41.6vh', left: '6.41vw' }}>cOnTROL LOg</span>
       
         <span className="custom-text right-flow" style={{ position: 'absolute', top: '35.4vh', left: '28.41vw' }}>0</span>
         <span className="custom-text right-flow" style={{ position: 'absolute', top: '41.6vh', left: '28.41vw' }}>0</span>

3379c335984510fce2eb0d4f64afa03f1a6f33cd
IOUL-IO <info@ioul.io>
2025-08-01 22:15:47 -0700

    Update page.tsx

--- app/tool-kit/lms/learn/page.tsx
+++ app/tool-kit/lms/learn/page.tsx
@@ -58,7 +58,6 @@ export default function Page() {
         <span className="custom-text" style={{position:'absolute',top:'41.6vh',left:'36vw',zIndex:4,fontFamily:"'Distill Expanded',sans-serif",color:'#111111',letterSpacing:'0.28vw',fontSize:'0.47rem',textShadow:'0.001rem 0.001rem 0 #717171,-0.001rem -0.001rem 0 #717171',}}>1NFO:</span>
 
         <span className="custom-text" style={{position:'absolute',top:'53vh',left:'36vw',zIndex:4,fontFamily:"'Distill Expanded',sans-serif",color:'#111111',letterSpacing:'0.28vw',fontSize:'0.47rem',textShadow:'0.001rem 0.001rem 0 #717171,-0.001rem -0.001rem 0 #717171',}}>L-1:</span>
-        <span className="custom-text" style={{position:'absolute',top:'53vh',left:'40vw',zIndex:4,fontFamily:"'Distill Expanded',sans-serif",color:'#111111',letterSpacing:'0.28vw',fontSize:'0.47rem',textShadow:'0.001rem 0.001rem 0 #717171,-0.001rem -0.001rem 0 #717171',}}>1nFO</span>
       
         <span className="custom-text" style={{position:'absolute',top:'53vh',left:'91vw',zIndex:4,fontFamily:"'Distill Expanded',sans-serif",color:'#111111',letterSpacing:'0.28vw',fontSize:'0.47rem',textShadow:'0.001rem 0.001rem 0 #717171,-0.001rem -0.001rem 0 #717171',}}>LOAD:</span>
       

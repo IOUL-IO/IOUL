b6ca4189121c79c3c7a7334c9094b37fe7e1e24f
IOUL-IO <info@ioul.io>
2025-08-04 16:06:38 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -272,9 +272,9 @@ const Page: React.FC = () => {
 
       {/* Account creation wrapper */}
       <div className="account-wrapper">
-        <span className="account-text account-email">E-MA1L ADDRESS</span>
-        <span className="account-text account-username">YOUR USERnAME</span>
-        <span className="account-text account-sign-password">YOUR PASSWORD</span>
+        <span className="account-text account-email">1nPUT E-MA1L</span>
+        <span className="account-text account-username">ADD USERnAME</span>
+        <span className="account-text account-sign-password">ADD PASSWORD</span>
         <div className="account-line account-line1" />
         <div className="account-line account-line2" />
         <div className="account-line account-line3" />
